04bda0944cd47f994a26ef429971bced60442116
Chadwick Wood <chadwick@coffeeshopped.com>
2025-03-24 21:02:27 -0500

    Added moduleTruss dirMap parsing.

--- PBJS/BasicModuleTruss.swift
+++ PBJS/BasicModuleTruss.swift
@@ -9,27 +9,14 @@ extension BasicModuleTruss: JsParsable {
       "editor" : ".d",
       "sections" : ".a",
     ], {
-      
       let editor: BasicEditorTruss = try $0.x("editor")
-      let sections: [ModuleTrussSection] = try $0.x("sections")
-      let manu: String = try $0.x("manu")
-      let subid: String = try $0.x("subid")
-      
-      let colorGuide: ColorGuide
-      if let arr = $0.forProperty("colorGuide"),
-         let arrS = arr.toArray() as? [String] {
-        colorGuide = ColorGuide(arrS)
-      }
-      else {
-        colorGuide = ColorGuide([
-          "#009f63",
-          "#ec421e",
-          "#717efe",
-          "#79f11e",
-        ])
-      }
-
-      return BasicModuleTruss(editor, manu: manu, model: editor.displayId, subid: subid, sections: sections, pathFn: nil, viewController: nil, dirMap: [:], colorGuide: colorGuide, indexPath: nil, configPaths: nil, postAddMsg: nil)
+      let colors: [String] = try $0.xq("colorGuide") ?? [
+        "#009f63",
+        "#ec421e",
+        "#717efe",
+        "#79f11e",
+      ]
+      return try BasicModuleTruss(editor, manu: $0.x("manu"), model: editor.displayId, subid: $0.x("subid"), sections: $0.x("sections"), pathFn: nil, viewController: nil, dirMap: $0.xq("dirMap") ?? [:], colorGuide: ColorGuide(colors), indexPath: nil, configPaths: nil, postAddMsg: nil)
     }),
   ]
 
